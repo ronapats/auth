42959eb2626432d852b9c1b5bf1ca403f30bd2f9
ronapats <ronapat.srisupavanich@kellogg.northwestern.edu>
2023-02-21 20:48:00 +0000

    practice

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+    def new
+    end
+    def create
+        #check if the user exists
+        @user = User.find_by({"email" => params["email"] })
+        if @user
+        #if they do, check they know their passwords
+            if params["password"] == @user ["password"]
+            #if they do, send them in
+            redirect_to "/companies"   
+            else
+            redirect_to "/sessions/new" 
+            end
+        else
+        redirect_to "/sessions/new"   
+        end
+    end
+end
